234c1a678a7a7ef4871030eca410ff98cff0cd16
Sophie Nichol <snichol00@stuy.edu>
2019-01-31 23:02:46 -0500

    added tolerance

--- Recursion.java
+++ Recursion.java
@@ -1,21 +1,21 @@
 public class Recursion{
-  public static double sqrt(double n){
-    return sqrt(n, n/2);
+  public static double sqrt(double n, double tolerance){
+    return sqrt(n, n/2, tolerance);
   }
 
-  public static double sqrt(double n, double guess){
-    if (Math.abs((n - (guess * guess)) / n) < 0.01){
+  public static double sqrt(double n, double guess, double tolerance){
+    if (Math.abs((n - (guess * guess)) / n) < (tolerance / 100)){
       return guess;
     }
-    return sqrt(n, (n / guess + guess) / 2);
+    return sqrt(n, (n / guess + guess) / 2, tolerance);
   }
 
   public static void main(String args[]){
-    System.out.println("2 = " + sqrt(4));
-    System.out.println("15 = " + sqrt(225));
-    System.out.println("0.0316 = " + sqrt(0.001));
-    System.out.println("20044.677 = " + sqrt(401789078));
-    System.out.println("0.94528 = " + sqrt(0.89356272));
-    System.out.println("3.58711 = " + sqrt(12.86738));
+    System.out.println("2 = " + sqrt(4, 0.1));
+    System.out.println("15 = " + sqrt(225, 0.0001));
+    System.out.println("0.0316 = " + sqrt(0.001, 0.0003));
+    System.out.println("20044.677 = " + sqrt(401789078, 0.005));
+    System.out.println("0.94528 = " + sqrt(0.89356272, 0.02376));
+    System.out.println("3.58711 = " + sqrt(12.86738, 0.00001));
   }
 }
